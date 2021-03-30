0c2649845cd0d3782dbfada005fcec69b5604da7
Sergei U <67503557+Sergei-U@users.noreply.github.com>
2021-03-30 08:47:33 +0300

    Add files via upload

---
+++ js.js
@@ -0,0 +1,15 @@
+<script>
+    	let button = document.querySelector('.popup-button');
+
+    	button.addEventListener('mouseover', showPopup);
+
+    	function showPopup() {
+        	document.querySelector('.popup').classList.add('visible');
+    	}
+
+    	button.addEventListener('mouseout', hidePopup);
+
+    	function hidePopup() {
+        	document.querySelector('.popup').classList.remove('visible');
+    	}
+	</script>
